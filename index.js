cc8c15fe7cda3d232ac99cf8fbf961f49e751084
Nicholas Levenson <nicklevenson@gmail.com>
2021-02-01 10:58:55 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let obj = array.find(function(e) {return e.result === "W"})
+  return (obj !== undefined) ? obj.year : undefined
+}
